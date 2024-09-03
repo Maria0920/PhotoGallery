94219ea38e581829bfb3a7ad301eb1b3c4da50f7
Saki <143759267+Maria0920@users.noreply.github.com>
2024-09-03 11:25:00 +1000

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -9,13 +9,14 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZUPjfQSO6UvNAOZ840K1ni4pD_iCzKII",
-  authDomain: "blogging-website-27f1f.firebaseapp.com",
-  projectId: "blogging-website-27f1f",
-  storageBucket: "blogging-website-27f1f.appspot.com",
-  messagingSenderId: "521028220081",
-  appId: "1:521028220081:web:c81eba924423979d92de90",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
